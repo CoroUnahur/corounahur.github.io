ce20b359a515b6cd7dbac4019c8e5b00c542e202
Branko Muruaga <muruagabranko253@gmail.com>
2025-01-01 21:15:23 -0300

    Se optimizaron las imagenes

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -5,72 +5,72 @@ export const images: Image[] = [
     {
         id:1,
         url: "images/image (1).webp",
-        srcPreview:"images/image (1).webp"
+        srcPreview:"images/preview/image (1).webp"
     }
     ,
     {
         id:2,
         url: "images/image (2).webp",
-        srcPreview:"images/image (2).webp"
+        srcPreview:"images/preview/image (2).webp"
     }
     ,
     {
         id:3,
         url: "images/image (3).webp",
-        srcPreview:"images/image (3).webp"
+        srcPreview:"images/preview/image (3).webp"
     }
     ,
     {
         id:4,
         url: "images/image (4).webp",
-        srcPreview:"images/image (4).webp"
+        srcPreview:"images/preview/image (4).webp"
     }
     ,
     {
         id:5,
         url: "images/image (5).webp",
-        srcPreview:"images/image (5).webp"
+        srcPreview:"images/preview/image (5).webp"
     }
     ,
     {
         id:6,
         url: "images/image (6).webp",
-        srcPreview:"images/image (6).webp"
+        srcPreview:"images/preview/image (6).webp"
     }
     ,
     {
         id:7,
         url: "images/image (7).webp",
-        srcPreview:"images/image (7).webp"
+        srcPreview:"images/preview/image (7).webp"
     }
     ,
     {
         id:8,
         url: "images/image (8).webp",
-        srcPreview:"images/image (8).webp"
+        srcPreview:"images/preview/image (8).webp"
     }
     ,
     {
         id:9,
         url: "images/image (9).webp",
-        srcPreview:"images/image (9).webp"
+        srcPreview:"images/preview/image (9).webp"
     }
     ,
     {
         id:10,
         url: "images/image (10).webp",
-        srcPreview:"images/image (10).webp",
+        srcPreview:"images/preview/image (10).webp",
     }
     ,
     {
         id:11,
         url: "images/image (11).webp",
-        srcPreview:"images/image (11).webp",
+        srcPreview:"images/preview/image (11).webp",
     }
     ,
     {
         id:12,
         url: "images/image (12).webp",
-        srcPreview:"images/image (12).webp",
+        srcPreview:"images/preview/image (12).webp",
     }
 ]
--- src/utils/reels.ts
+++ src/utils/reels.ts
@@ -3,23 +3,23 @@ import type { Reel } from "./types";
 export const reels: Reel[] = [
 
     {
-        src: 'https://instagram.faep7-1.fna.fbcdn.net/o1/v/t16/f2/m86/AQOlRDba8c41FwQ0XdS03xOR-ihfSJFayP6efxg4aDoyO3g7D7If5iHAObEN-L681nxiplClxqT3I0Y7Vyj0ZNIhVtcxCMidS0Lh_B8.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_cat=104&vs=1063259025278905_2409760064&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC81NzQ0NDc1MjAwMzM0MzE0QUM3MEQyQzNCMUI0RDQ5Ml92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dQbWQzeHVPcmhHNGZONEZBQVdxYnVhblBMTnhicV9FQUFBRhUCAsgBACgAGAAbABUAACbIgNnkv%2BCEQBUCKAJDMywXQEyAAAAAAAAYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&ccb=9-4&oh=00_AYDhW6Fxwy_uro-ZLrdXZ3H-EhR1oj593I3cWzM6lY9EsQ&oe=675E7124&_nc_sid=7a9f4b',
-        srcPreview: 'reels/reel1.jpg'
+        src: 'reels/reel (1).mp4',
+        srcPreview: 'reels/reel1.webp'
     },
     {
-        src: 'https://instagram.faep7-1.fna.fbcdn.net/o1/v/t16/f2/m86/AQNLlGVFD1mTlcB51Ya_a1z0xDRwaSVS4aDHRkI8DtoRSRWJKl9Iv7bWK4IiwqmbE0Iu9BF27Nx5V6eq9i9YguRqX8R98KAbLgCdh1E.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_cat=102&vs=1665025584042625_3226749991&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC8xMjQwMEM4RDYyMDU4NDE1Nzc5RDBCQTdGNUQ4OEQ5Nl92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dCaFgweHRfTUlITXJVQUJBR0FQYXlfaXkwRnlicV9FQUFBRhUCAsgBACgAGAAbABUAACb%2BtvHM0NSDQBUCKAJDMywXQDa7peNT988YEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&ccb=9-4&oh=00_AYDdS2MuNfALnnAe4G8VyqyDDDxPevMHElgm8gE-NMMXog&oe=675E83B5&_nc_sid=7a9f4b',
-        srcPreview: 'reels/reel2.jpg'
+        src: 'reels/reel (2).mp4',
+        srcPreview: 'reels/reel2.webp'
     },
     {
-        src: 'https://instagram.faep7-1.fna.fbcdn.net/o1/v/t16/f2/m86/AQOYMB845oXadain_JVIY5MZDCqSAFXtqncvaseUdwz1NGxVMnU-q_m5GRDaRS1TDxHOkt7hp2OTiLrJHwSwZaJ2T6MASYf-hQbAEy8.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_cat=103&vs=835610345420295_470158594&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC9DMzQyRjk3NkZDRERENDVBQzNENzlBOTkyMzQwRDJCQV92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dNd1BjQnZnanZ2UWRfNEJBTkttQzFuSjNZZzdicV9FQUFBRhUCAsgBACgAGAAbABUAACb0tqLBlvP8PxUCKAJDMywXQGeTMzMzMzMYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&ccb=9-4&oh=00_AYDx_yslLJvIICOwRtBbcJ6hnugQO3mPI9zvmUfyWORiRw&oe=675E84B0&_nc_sid=7a9f4b',
-        srcPreview: 'reels/reel3.jpg'
+        src: 'reels/reel (3).mp4',
+        srcPreview: 'reels/reel3.webp'
     },
     {
-        src: 'https://instagram.faep7-1.fna.fbcdn.net/o1/v/t16/f2/m86/AQMCP8GIHlpHpQrvV1hpHMt1SmfM_9Rv898INKUEYCNQ44zMcbJyX6vpFDidzxajSwivOABQ4Gf51cx58UoCaTK321XVPc4yvenruE4.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_cat=101&vs=1699250113970274_3259543158&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC81NjQzMzQ5RjMwMzBCRkFCMTJDMEQ5RTUwQzMzMUFCNl92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dMYXc4QnRtamNnWWNPNENBSzlXbHctVjdhWXpicV9FQUFBRhUCAsgBACgAGAAbABUAACa2yNjehbD4PxUCKAJDMywXQEJMzMzMzM0YEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&ccb=9-4&oh=00_AYA8pBcxXifb5gRWxPkLoI7BDlYx1Uyn-oLY-BO3z9rtmQ&oe=675E9ED9&_nc_sid=7a9f4b',
-        srcPreview: 'reels/reel4.jpg'
+        src: 'reels/reel (4).mp4',
+        srcPreview: 'reels/reel4.webp'
     },
     {
-        src: 'https://instagram.faep7-1.fna.fbcdn.net/o1/v/t16/f2/m86/AQOYMB845oXadain_JVIY5MZDCqSAFXtqncvaseUdwz1NGxVMnU-q_m5GRDaRS1TDxHOkt7hp2OTiLrJHwSwZaJ2T6MASYf-hQbAEy8.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNzIwLmJhc2VsaW5lIn0&_nc_cat=103&vs=835610345420295_470158594&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC9DMzQyRjk3NkZDRERENDVBQzNENzlBOTkyMzQwRDJCQV92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dNd1BjQnZnanZ2UWRfNEJBTkttQzFuSjNZZzdicV9FQUFBRhUCAsgBACgAGAAbABUAACb0tqLBlvP8PxUCKAJDMywXQGeTMzMzMzMYEmRhc2hfYmFzZWxpbmVfMV92MREAdf4HAA%3D%3D&ccb=9-4&oh=00_AYDx_yslLJvIICOwRtBbcJ6hnugQO3mPI9zvmUfyWORiRw&oe=675E84B0&_nc_sid=7a9f4b',
-        srcPreview: 'reels/reel3.jpg'
+        src: 'reels/reel (5).mp4',
+        srcPreview: 'reels/reel5.webp'
     },
 ];

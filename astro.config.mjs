4681d7bd4ad1314c36f0a595b33205892ba37f59
Branko Muruaga <muruagabranko253@gmail.com>
2025-01-02 23:41:44 -0300

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  image: {
+    service: passthroughImageService(),
+  },
+  site: "https://corounahur.github.io",
+  integrations: [tailwind()],
 });
